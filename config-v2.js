cd94bb7a847c8fae6116795d6a40a5612afb39e9
kingdom zeus <110291716+kingdomzeus@users.noreply.github.com>
2024-10-03 08:26:13 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94756260023"
 const SESSION_ID = ""
 
 
